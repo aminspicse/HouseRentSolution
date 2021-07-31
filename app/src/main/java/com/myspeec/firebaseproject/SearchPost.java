10a60ed40fb9b0046f633d0ed4f94344a4b172bf
aminspicse@gmail.com <Amin766855>
2021-07-31 12:26:16 +0600

    Added Search

---
+++ app/src/main/java/com/myspeec/firebaseproject/SearchPost.java
@@ -0,0 +1,14 @@
+package com.myspeec.firebaseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_post);
+    }
+}
