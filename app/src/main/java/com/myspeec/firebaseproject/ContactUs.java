b7f65d9ae8d05c3a369c4da9e832dd028a727dbb
aminspicse@gmail.com <Amin766855>
2021-07-31 12:24:14 +0600

    Added Contuct Us

---
+++ app/src/main/java/com/myspeec/firebaseproject/ContactUs.java
@@ -0,0 +1,17 @@
+package com.myspeec.firebaseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_us);
+        getSupportActionBar().setTitle("Contact Us");
+
+
+    }
+}
