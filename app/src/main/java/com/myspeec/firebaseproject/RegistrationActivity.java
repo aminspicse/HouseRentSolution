487a6bbed7f2bc0416cafa2142a5e56a7121aeb1
aminspicse@gmail.com <Amin766855>
2021-08-02 10:27:37 +0600

    Updated

--- app/src/main/java/com/myspeec/firebaseproject/RegistrationActivity.java
+++ app/src/main/java/com/myspeec/firebaseproject/RegistrationActivity.java
@@ -19,33 +19,37 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+// reference Link: https://www.youtube.com/watch?v=exo7NwC2joE&list=PLeSuKd7QCa4bcla2vqbP3D6Uriw1RAvDA&index=6
 
 public class RegistrationActivity extends AppCompatActivity {
-//https://www.youtube.com/watch?v=exo7NwC2joE&list=PLeSuKd7QCa4bcla2vqbP3D6Uriw1RAvDA&index=6
+
+    // global variable declaration
+
     EditText editTextEmail, editTextPassword, editTextConfirmPassword;
     Button btnRegister;
     TextView textviewLogin, editTextError;
     String Email, Password;
     FirebaseAuth fireBaseAuth;
-
     ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        // for hide actionbar
         getSupportActionBar().hide();
 
+        // get data from xml by id
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
         editTextError = findViewById(R.id.editTextError);
-
         btnRegister = findViewById(R.id.btnRegister);
-
         textviewLogin = findViewById(R.id.textviewLogin);
-
         progressBar = findViewById(R.id.progressBar);
+
+        // when user click login then action this code
 
         textviewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,55 +62,19 @@ public class RegistrationActivity extends AppCompatActivity {
 
         fireBaseAuth = FirebaseAuth.getInstance();
 
+        // when user click registration button then work this function
+
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 progressBar.setVisibility(View.VISIBLE); // for progress bar visibility
 
-                if(!EmailValidation() | !PasswordValidation() | !ConfirmPasswordValidation()){
-                    progressBar.setVisibility(View.GONE);
-                    return;
-                }else{
-                    Email = editTextEmail.getText().toString();
-                    Password = editTextPassword.getText().toString();
-
-                    fireBaseAuth.createUserWithEmailAndPassword(Email,Password)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if(task.isSuccessful()){
-                                        fireBaseAuth.getCurrentUser().sendEmailVerification()
-                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                    @Override
-                                                    public void onComplete(@NonNull Task<Void> task) {
-                                                        if(task.isSuccessful()){
-                                                            Toast.makeText(RegistrationActivity.this, "Successfull", Toast.LENGTH_LONG).show();
-                                                            startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
-                                                        }
-                                                    }
-                                                });
-                                    }
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d("This",e.getMessage());
-                            if(e.getMessage().equals("The email address is already in use by another account.")){
-                                editTextEmail.setError("The email address is already in use by another account.");
-                                editTextError.setText("The email address is already in use by another account.");
-                            }else if(e.getMessage().equals("The email address is badly formatted.")){
-                                editTextError.setText("The email address is badly formatted.");
-                                editTextEmail.setError("The email address is badly formatted.");
-                            }
-
-                            progressBar.setVisibility(View.GONE);
-                        }
-                    });
-                }
+                Registration();
             }
         });
     }
 
+    // for validating user email
     boolean EmailValidation(){
         String email = editTextEmail.getText().toString();
         if(email.isEmpty()){
@@ -121,6 +89,7 @@ public class RegistrationActivity extends AppCompatActivity {
         }
     }
 
+    // for valiating user password
     boolean PasswordValidation(){
         String password = editTextPassword.getText().toString();
         if(password.isEmpty()){
@@ -135,6 +104,7 @@ public class RegistrationActivity extends AppCompatActivity {
         }
     }
 
+    //Validate password and confirm passwrod for user
     boolean ConfirmPasswordValidation(){
         String password = editTextPassword.getText().toString();
         String confirmpassword = editTextConfirmPassword.getText().toString();
@@ -151,6 +121,49 @@ public class RegistrationActivity extends AppCompatActivity {
         }else{
             editTextConfirmPassword.setError(null);
             return true;
+        }
+    }
+
+    public void Registration(){
+        if(!EmailValidation() | !PasswordValidation() | !ConfirmPasswordValidation()){
+            progressBar.setVisibility(View.GONE);
+            return;
+        }else{
+            Email = editTextEmail.getText().toString().toLowerCase();
+            Password = editTextPassword.getText().toString();
+
+            fireBaseAuth.createUserWithEmailAndPassword(Email,Password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                fireBaseAuth.getCurrentUser().sendEmailVerification()
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()){
+                                                    Toast.makeText(RegistrationActivity.this, "Successfull", Toast.LENGTH_LONG).show();
+                                                    startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                                                }
+                                            }
+                                        });
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("This",e.getMessage());
+                    if(e.getMessage().equals("The email address is already in use by another account.")){
+                        editTextEmail.setError("The email address is already in use by another account.");
+                        editTextError.setText("The email address is already in use by another account.");
+                    }else if(e.getMessage().equals("The email address is badly formatted.")){
+                        editTextError.setText("The email address is badly formatted.");
+                        editTextEmail.setError("The email address is badly formatted.");
+                    }
+
+                    progressBar.setVisibility(View.GONE);
+                }
+            });
         }
     }
 }
